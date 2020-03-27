71b10b81a2621f6aa0ca225abadd577c645caca2
Jose Armesto <github@armesto.net>
2020-03-27 09:21:19 +0100

    Config comes from secret

--- src/main/java/io/giantswarm/azurevaultdemo/HelloController.java
+++ src/main/java/io/giantswarm/azurevaultdemo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${marco}")
+    @Value("${marco:not secret}")
     private String marcoLastName;
 
     @RequestMapping("/")
